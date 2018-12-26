c0264753376e38bde1c62ccee1612cd80fdd7078
Ubaid <ubaid.rehman07@gmail.com>
2018-12-26 19:08:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 //Install express server
 const express = require('express');
+const http = require('http');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
