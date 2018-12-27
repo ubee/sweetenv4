3b6ce638e86fe20250dbd437b61840277379128a
Ubaid <ubaid.rehman07@gmail.com>
2018-12-27 16:40:14 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.set('port', port);
 
 const server = http.createServer(app);
